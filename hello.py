aea8ee067e472aeb34af7cb5d2c5600736335fa1
= <miles.lincoln@gmail.com>
2012-10-30 11:00:39 -0500

    got rid of login...try #2

--- hello.py
+++ hello.py
@@ -17,23 +17,23 @@ class Main(flask.views.MethodView):
     def get(self):
         return flask.render_template('index.html')
     
-    def post(self):
-        if 'logout' in flask.request.form:
-            flask.session.pop('username', None)
-            return flask.redirect(flask.url_for('index'))
-        required = ['username', 'passwd']
-        for r in required:
-            if r not in flask.request.form:
-                flask.flash("Error: {0} is required.".format(r))
-                return flask.redirect(flask.url_for('index'))
-        username = flask.request.form['username']
-        passwd = flask.request.form['passwd']
-        if username in users and users[username] == passwd:
-            flask.session['username'] = username
-        else:
-            flask.flash("Username doesn't exist or incorrect password")
-        return flask.redirect(flask.url_for('index'))
-
+#     def post(self):
+#         if 'logout' in flask.request.form:
+#             flask.session.pop('username', None)
+#             return flask.redirect(flask.url_for('index'))
+#         required = ['username', 'passwd']
+#         for r in required:
+#             if r not in flask.request.form:
+#                 flask.flash("Error: {0} is required.".format(r))
+#                 return flask.redirect(flask.url_for('index'))
+#         username = flask.request.form['username']
+#         passwd = flask.request.form['passwd']
+#         if username in users and users[username] == passwd:
+#             flask.session['username'] = username
+#         else:
+#             flask.flash("Username doesn't exist or incorrect password")
+#         return flask.redirect(flask.url_for('index'))
+# 
 # def login_required(method):
 #     @functools.wraps(method)
 #     def wrapper(*args, **kwargs):
@@ -45,11 +45,11 @@ class Main(flask.views.MethodView):
 #     return wrapper
 
 class Remote(flask.views.MethodView):
-    @login_required
+#     @login_required
     def get(self):
         return flask.render_template('remote.html')
         
-    @login_required
+#     @login_required
     def post(self):
         input = flask.request.form['expression']
         result = prepare(input)
