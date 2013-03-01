a8f786d9a76feb92c029506826d0c23c1927f2af
= <miles.lincoln@gmail.com>
2013-03-01 15:58:08 -0600

    working toward pep8 compliance

--- hello.py
+++ hello.py
@@ -1,13 +1,15 @@
-import flask, flask.views
+import flask
+import flask.views
 import os
 import functools
 import urllib2
 import re
 import operator
 import webbrowser
+import sys
 from datetime import datetime
-opener=urllib2.build_opener()
-opener.addheaders=[('User-agent', 'Mozilla/5.0')]
+opener = urllib2.build_opener()
+opener.addheaders = [('User-agent', 'Mozilla/5.0')]
 app = flask.Flask(__name__)
 app.secret_key = "bacon"
 
@@ -21,9 +23,10 @@ app.secret_key = "bacon"
 #users = {'user':'pass'}
 
 class Main(flask.views.MethodView):
+
     def get(self):
         return flask.render_template('remote.html')
-    
+
 #     def post(self):
 #         if 'logout' in flask.request.form:
 #             flask.session.pop('username', None)
@@ -40,7 +43,7 @@ class Main(flask.views.MethodView):
 #         else:
 #             flask.flash("Username doesn't exist or incorrect password")
 #         return flask.redirect(flask.url_for('index'))
-# 
+
 # def login_required(method):
 #     @functools.wraps(method)
 #     def wrapper(*args, **kwargs):
@@ -55,16 +58,16 @@ class Remote(flask.views.MethodView):
 #     @login_required
     def get(self):
         return flask.render_template('remote.html')
-        
+
 #     @login_required
     def post(self):
         input = flask.request.form['expression']
 ##        result = q.enqueue(prepare, input)
         result = prepare(input)
-##        result = eval(flask.request.form['expression'])      
-        flask.flash(result)
-        return flask.redirect(flask.url_for('remote'))
-    
+##        result = eval(flask.request.form['expression'])
+        #flask.flash(result)
+        return flask.render_template('remote.html', result=result)
+
 app.add_url_rule('/',
                  view_func=Main.as_view('index'),
                  methods=["GET", "POST"])
@@ -74,69 +77,80 @@ app.add_url_rule('/remote/',
 
 def prepare(wikiurl):
 ##    resp = requests.get(wikiurl)
-    wikiurl = wikiurl.replace("%","%25")
-    wikiurl = wikiurl.replace("'","%27")
-    wikiurl = wikiurl.replace("&","%26")
+    sys.stdout.write("***prepare***\n")
+    sys.stdout.flush()
+    wikiurl = wikiurl.replace("%", "%25")
+    wikiurl = wikiurl.replace("'", "%27")
+    wikiurl = wikiurl.replace("&", "%26")
     startTime = datetime.now()
     offset = ""
     matchlist = ""
     matchdict = {}
     monthdict = {}
     totalmatches = 0
-    output="Profiling the "+wikiurl+" page...\n"
-    return scrapewiki(wikiurl,offset,matchlist,matchdict,totalmatches,startTime,output,monthdict)
+    output = "Profiling the "+wikiurl+" page...\n"
+    return scrapewiki(wikiurl, offset, matchlist, matchdict, totalmatches, startTime, output, monthdict)
 
-def scrapewiki(wikiurl,offset,matchlist,matchdict,totalmatches,startTime,output,monthdict):
-    matchesonpage=0
-    url="http://en.wikipedia.org/w/index.php?title="+wikiurl+"&offset="+offset+"&limit=500&action=history"
-    page=opener.open(url)
+def scrapewiki(wikiurl, offset, matchlist, matchdict, totalmatches, startTime, output, monthdict):
+    sys.stdout.write("***scrapewiki***\n")
+    sys.stdout.flush()
+    matchesonpage = 0
+    url = "http://en.wikipedia.org/w/index.php?title="+wikiurl+"&offset="+offset+"&limit=500&action=history"
+    page = opener.open(url)
     while True:
-        currentline=page.readline()
-        if re.search(wikiurl+'\&amp;offset=(\d{14})',currentline):
-            offset=re.search(wikiurl+'\&amp;offset=(\d{14})',currentline).group(1)
-        if re.search(r'mw-changeslist-date">(\d{2}:\d{2}),\s{1}(\d{1,2})\s{1}(\w{3,10})\s{1}(\d{4})',currentline):
-            matchesonpage+=1
-            edittimestamp=re.search(r'mw-changeslist-date">(\d{2}:\d{2}),\s{1}(\d{1,2})\s{1}(\w{3,10})\s{1}(\d{4})',currentline)
+        currentline = page.readline()
+        if re.search(wikiurl+'\&amp;offset=(\d{14})', currentline):
+            offset = re.search(wikiurl+'\&amp;offset=(\d{14})', currentline).group(1)
+        if re.search(r'mw-changeslist-date">(\d{2}:\d{2}),\s{1}(\d{1,2})\s{1}(\w{3,10})\s{1}(\d{4})', currentline):
+            matchesonpage += 1
+            edittimestamp = re.search(r'mw-changeslist-date">(\d{2}:\d{2}),\s{1}(\d{1,2})\s{1}(\w{3,10})\s{1}(\d{4})', currentline)
             time = edittimestamp.group(1)
             day = edittimestamp.group(2)
             month = edittimestamp.group(3)
-            monthlist=["January","February","March","April","May","June","July","August","September","October","November","December"]
+            monthlist = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
             if month in monthlist:
                 month = "%02d" % (monthlist.index(month)+1)
             year = edittimestamp.group(4)
+            if len(str(month)) > 2:
+                sys.stdout.write("***month is fucked***\n")
+            if len(str(year)) < 4:
+                sys.stdout.write("***year is effed***\n")
             ddmmyyyy = str(day+"-"+month+"-"+year)
             yyyymm = str(year+"-"+month)
+            sys.stdout.write(yyyymm+"\n")
             if ddmmyyyy in matchdict:
-                matchdict[ddmmyyyy]+=1
+                matchdict[ddmmyyyy] += 1
             else:
-                matchdict[ddmmyyyy]=1
+                matchdict[ddmmyyyy] = 1
             if yyyymm in monthdict:
-                monthdict[yyyymm]+=1
+                monthdict[yyyymm] += 1
             else:
-                monthdict[yyyymm]=1
+                monthdict[yyyymm] = 1
             matchlist += time + "\t" + day + "\t" + month + "\t" + year + "\n"
             totalmatches += 1
-        if len(currentline)==0:
+        if len(currentline) == 0:
 ##            output += "matches found on first page: "+str(matchesonpage)+"\n"
-            if matchesonpage>=499 and offset!="":
-                return recursion(wikiurl,offset,matchlist,matchdict,totalmatches,startTime,output,monthdict)
+            if matchesonpage >= 499 and offset != "":
+                return recursion(wikiurl, offset, matchlist, matchdict, totalmatches, startTime, output, monthdict)
                 break
             else:
-                return dumpresults(wikiurl,offset,matchlist,matchdict,totalmatches,startTime,output,monthdict)
+                return dumpresults(wikiurl, offset, matchlist, matchdict, totalmatches, startTime, output, monthdict)
                 break
 
-def recursion(wikiurl,offset,matchlist,matchdict,totalmatches,startTime,output,monthdict):
-    url="http://en.wikipedia.org/w/index.php?title="+wikiurl+"&offset="+offset+"&limit=500&action=history"
-    page=opener.open(url)
-    matchesonpage=0
+def recursion(wikiurl, offset, matchlist, matchdict, totalmatches, startTime, output, monthdict):
+    sys.stdout.write("***recursion***\n")
+    sys.stdout.flush()
+    url = "http://en.wikipedia.org/w/index.php?title="+wikiurl+"&offset="+offset+"&limit=500&action=history"
+    page = opener.open(url)
+    matchesonpage = 0
     while True:
-        currentline=page.readline()
-        if re.search(wikiurl+'\&amp;offset=(\d{14})',currentline):
-            if re.search(wikiurl+'\&amp;offset=(\d{14})',currentline).group(1)<offset:
-                offset=re.search(wikiurl+'\&amp;offset=(\d{14})',currentline).group(1)
-        if re.search(r'mw-changeslist-date">(\d{2}:\d{2}),\s{1}(\d{1,2})\s{1}(\w{3,10})\s{1}(\d{4})',currentline):
-            matchesonpage+=1
-            edittimestamp=re.search(r'mw-changeslist-date">(\d{2}:\d{2}),\s{1}(\d{1,2})\s{1}(\w{3,10})\s{1}(\d{4})',currentline)
+        currentline = page.readline()
+        if re.search(wikiurl+'\&amp;offset=(\d{14})', currentline):
+            if re.search(wikiurl+'\&amp;offset=(\d{14})', currentline).group(1) < offset:
+                offset = re.search(wikiurl+'\&amp;offset=(\d{14})', currentline).group(1)
+        if re.search(r'mw-changeslist-date">(\d{2}:\d{2}),\s{1}(\d{1,2})\s{1}(\w{3,10})\s{1}(\d{4})', currentline):
+            matchesonpage += 1
+            edittimestamp = re.search(r'mw-changeslist-date">(\d{2}:\d{2}),\s{1}(\d{1,2})\s{1}(\w{3,10})\s{1}(\d{4})', currentline)
             time = edittimestamp.group(1)
             day = edittimestamp.group(2)
             month = edittimestamp.group(3)
@@ -156,21 +170,15 @@ def recursion(wikiurl,offset,matchlist,matchdict,totalmatches,startTime,output,m
             matchlist += time + "\t" + day + "\t" + month + "\t" + year + "\n"
             totalmatches += 1
         if len(currentline)==0 and matchesonpage<499:
-##            output += "matches on final page: "+str(matchesonpage)+"\n"
-##            output += "A total of "+str(totalmatches)+" edits have been made to this page.\n"
             return dumpresults(wikiurl,offset,matchlist,matchdict,totalmatches,startTime,output,monthdict)
             break
         if len(currentline)==0:
-##            output += "matches found on this page: "+str(matchesonpage)+"\n"
-##            output += "matches found so far: "+str(totalmatches)+"\n"
             if matchesonpage>=499:
-##                output += "going to the next page\n"
                 return recursion(wikiurl,offset,matchlist,matchdict,totalmatches,startTime,output,monthdict)
                 break
             
 def dumpresults(wikiurl,offset,matchlist,matchdict,totalmatches,startTime,output,monthdict):
     sortdict = (sorted(matchdict.iteritems(), key=operator.itemgetter(1), reverse=True))
-##    print sortdict
     output+="A total of "+str(totalmatches)+" edits have been made to this page\n"
     maxeditday = max(matchdict.iteritems(), key=operator.itemgetter(1))[0]
     output += "The highest number of edits ("+ str(matchdict[maxeditday]) + ') to the <a href="http://en.wikipedia.org/wiki/'+wikiurl+'">'+wikiurl+"</a> page occurred on " + str(maxeditday) + " (dd/mm/yyyy).\n"
@@ -204,6 +212,9 @@ def dumpresults(wikiurl,offset,matchlist,matchdict,totalmatches,startTime,output
     htmltable += "</table>"
 
     output += htmltable
+    sys.stdout.write("Output is: " + output+"\n")
+    sys.stdout.write(str(monthdict)+"\n")
+    sys.stdout.flush()
     return flask.Markup(output)
     
 port = int(os.environ.get('PORT', 5000))
